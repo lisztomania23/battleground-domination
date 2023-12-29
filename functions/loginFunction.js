736259a634a97fef3c18c0a15738edcf92a1719e
lisztomania23 <97365025+lisztomania23@users.noreply.github.com>
2023-12-29 12:08:13 +0530

    Update loginFunction.js

--- functions/loginFunction.js
+++ functions/loginFunction.js
@@ -1,24 +1,8 @@
-exports.handler = async (event, context) => {
-  try {
-    const data = JSON.parse(event.body);
-    const { username, password } = data;
+// functions/loginFunction.js
 
-    // Replace this with your actual authentication logic
-    if (username === 'demo' && password === 'password') {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Login successful' }),
-      };
-    } else {
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ message: 'Login failed' }),
-      };
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Internal server error' }),
-    };
-  }
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello from Netlify function' }),
+  };
 };
