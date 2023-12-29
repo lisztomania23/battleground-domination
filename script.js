81d4acf994685943b4eff549914fc678d3497e22
lisztomania23 <97365025+lisztomania23@users.noreply.github.com>
2023-12-29 06:02:02 +0530

    added: js for login

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+      const response = await fetch('/.netlify/functions/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const result = await response.json();
+      console.log(result);
+
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  });
+};
