1a3408dbc51a760c60d9fd5f0848be67a783c6fd
lisztomania23 <97365025+lisztomania23@users.noreply.github.com>
2023-12-29 12:16:47 +0530

    Update loginFunction.js

--- functions/loginFunction.js
+++ functions/loginFunction.js
@@ -1,8 +1,19 @@
 // functions/loginFunction.js
 
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Netlify function' }),
-  };
+  try {
+    const data = JSON.parse(event.body);
+    const { username, password } = data;
+
+    // For testing purposes, always consider the login as successful
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Login successful', username }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error' }),
+    };
+  }
 };
